bedda4a0d632c10650fec60a2bb7ea8bf23aef93
gragra-kj <muthuigracia27@gmail.com>
2024-06-24 14:53:26 +0300

    loops

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        final byte MONTH=12;
+
         int pmt=0;
         float rate=0;
-        float monthlyRate=0;
-        int periodInMonths=0;
-        final byte PERCENT=100;
+        int period=0;
         System.out.println("Future Value Calculator");
         Scanner scanner=new Scanner(System.in);
         while (true){
@@ -22,26 +20,36 @@ public class Main {
             System.out.print("Annual interest Rate: ");
             rate=scanner.nextFloat();
             if (rate >= 0 && rate <=30) {
-                monthlyRate=rate / MONTH /PERCENT;
+
                 break;
             }
             System.out.println("Enter a value between 0 and 70");
         }
         while (true){
             System.out.print("Number of Years/Period: ");
-            int period= scanner.nextInt();
+            period= scanner.nextInt();
             if (period >=1 && period<=30){
-                periodInMonths=period * MONTH;
+
                 break;
 
             }
             System.out.println("Enter a value between 1 and 30");
 
         }
-
-        double futureValue=pmt*((Math.pow(1+monthlyRate,periodInMonths)-1)/(monthlyRate));
+        double futureValue=calculateFv(pmt,rate,period);
         String formatedFureValue= NumberFormat.getCurrencyInstance().format(futureValue);
         System.out.println("Future Value: " +formatedFureValue);
+    }
+    public static double calculateFv(int pmt,float rate,int period){
+        final byte MONTH=12;
+        final byte PERCENT=100;
+        float monthlyRate=rate / MONTH /PERCENT;
+        int periodInMonths=period * MONTH;
+        double futureValue=pmt*((Math.pow(1+monthlyRate,periodInMonths)-1)/(monthlyRate));
+        return futureValue;
+
+
+
     }
 
 }
